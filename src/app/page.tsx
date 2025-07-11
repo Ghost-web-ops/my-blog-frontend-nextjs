f5c2bfb8084cdfa3f166bca110c5df3047ef9612
Ghost-web-ops <omaryasser011549@gmail.com>
2025-07-11 16:47:50 +0300

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default async function Home() {
         Blog
       </h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        {posts && posts.filter(post => post).map((post) => (
+       {posts && posts.filter(post => post && post.attributes).map((post) => (
   <PostCard key={post.id} post={post} />
 ))}
       </div>
